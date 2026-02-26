badd444e9a90b654bfa59ecd15d7cc5c0d251234
Abhilasha Pohekar <pohekarabhilasha@gmail.com>
2026-02-26 17:11:53 +0000

    initial commit

---
+++ src/com/abhilasha/basics/VariableDemo.java
@@ -0,0 +1,30 @@
+package com.abhilasha.basics;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+
+		// data types basics
+
+		System.out.println("Hello World !!!");
+
+		// primitive data types
+		byte b = (byte) 127;
+		boolean bool = false;
+		char c = 'a';
+		short s = 234;
+		int i = 289;
+		float f = 54.888887f;
+		long l = 299;
+		double d = 23.898945378;
+
+		System.out.println(b);
+		System.out.println(bool);
+		System.out.println(c);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(d);
+	}
+}
